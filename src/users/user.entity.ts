a6269fc4558a8da019f0fde45f31b3c590c95677
Ameni <ameni.hajri@esprit.tn>
2025-12-07 12:51:43 +0100

    adding hooks in entity user

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, ObjectIdColumn, ObjectId, Column, BeforeInsert } from 'typeorm';
+import { Entity, ObjectIdColumn, ObjectId, Column, BeforeInsert, AfterUpdate, AfterInsert, BeforeRemove, AfterLoad } from 'typeorm';
 import { IsString, IsEmail, IsBoolean } from "class-validator";
 import { Logger } from '@nestjs/common';
 @Entity()
@@ -19,4 +19,26 @@ password: string;
 @Column({ default: false })
 @IsBoolean()
 active: boolean;
+
+@BeforeInsert()
+logInsert() {
+Logger.log('Creating User entity instance', 'UserEntity');
+}
+@AfterInsert()
+logAfterInsert() {
+Logger.log('User entity instance created', 'UserEntity');
+}
+@AfterUpdate()
+logAfterUpdate() {
+Logger.log('User entity instance updated', 'UserEntity');
+}
+@BeforeRemove()
+logBeforeRemove() {
+Logger.log(`Suppression de l'utilisateur avec l'id : ${this.id}`,
+'UserEntity');
+}
+@AfterLoad()
+logAfterLoad() {
+Logger.log(`Utilisateur récupéré : ${this.id}`, 'UserEntity');
+}
 }
